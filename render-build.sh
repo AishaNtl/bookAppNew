f1cb62db7d8f9b91a43289fda683a924d5f53c2c
AishaNtl <aishantuli003@gmail.com>
2025-07-16 19:31:44 +0100

    Solid queue fix

--- render-build.sh
+++ render-build.sh
@@ -3,12 +3,13 @@ set -o errexit
 
 # Create SQLite storage
 mkdir -p storage
-chmod -R 777 storage
 touch storage/production.sqlite3
+touch storage/production_queue.sqlite3
 
 # Install gems
 bundle config set without 'development test'
 bundle install
+bundle exec rails db:migrate
 
 # Run migrations
 bundle exec rails db:migrate RAILS_ENV=production
